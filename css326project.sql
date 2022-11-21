9f69123064c1455f1d505eeade9a3ab3867c8396
Raliciel <33251984+Raliciel@users.noreply.github.com>
2022-11-22 00:22:47 +0700

    database update

--- css326project.sql
+++ css326project.sql
@@ -24,18 +24,21 @@ SET time_zone = "+00:00";
 DROP DATABASE IF EXISTS css326project;
 CREATE DATABASE IF NOT EXISTS css326project;
 USE css326project;
--- --------------------------------------------------------
-
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Table structure for table `account`
---
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2022 at 06:20 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 CREATE TABLE `account` (
   `user_id` int(10) NOT NULL,
   `f_name` varchar(30) DEFAULT NULL,
   `l_name` varchar(30) DEFAULT NULL,
-  `username` varchar(30) DEFAULT NULL,
-  `passwd` varchar(30) DEFAULT NULL,
+  `username` varchar(30) NOT NULL,
+  `passwd` varchar(30) NOT NULL,
   `email` varchar(30) DEFAULT NULL,
   `user_picture` longblob DEFAULT NULL,
   `type` varchar(13) DEFAULT NULL,
@@ -48,8 +51,11 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`user_id`, `f_name`, `l_name`, `username`, `passwd`, `email`, `user_picture`, `type`, `admin_id`, `regis_date`) VALUES
-(35, 'Pimpika', 'Hongsawadee', '6322773340@g.siit.tu.ac.th', 'r9MT*kdoQ&f', '6322773340@g.siit.tu.ac.th', '', 'Teacher', NULL, '2022-10-24'),
-(36, 'พิมพิกา', 'หงษาวดี', 'th.pimpika@gmail.com', 'WXAv#9lhcM2', 'th.pimpika@gmail.com', '', 'Teacher', NULL, '2022-10-24');
+(0, 'admin', 'admin', 'admin', 'asdfg', 'admin@gmail.com', NULL, 'Admin', 0, '2022-11-17'),
+(38, 'พิมพิกา', 'หงษาวดี', 'th.pimpika@gmail.com', 'sss', 'th.pimpika@gmail.com', 0x31613732313131306630393931336131333062383036623161343439663762612e706e67, 'Admin', 0, '2022-10-24'),
+(41, 'ssss', 'ssss', 'ssss', 'w%e1mHKdG(j', 'ssss', '', 'Admin', 38, '2022-11-14'),
+(43, 'teacher', 'aa', 'teacher@gmail.com', 'bbb', 'teacher@gmail.com', 0x323630352d332e6a7067, 'Teacher', 0, '2022-11-16'),
+(47, 'aaa', 'aaa', 'teacher@hotmail.com', '3C#67Qr3QMC', 'teacher@hotmail.com', '', 'Teacher', 38, '2022-11-17');
 
 -- --------------------------------------------------------
 
@@ -83,6 +89,17 @@ CREATE TABLE `course` (
   `regis_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `course_name`, `grade_to_learn`, `teacher_id`, `admin_id`, `regis_date`) VALUES
+('GTS231', 'Physic', 10, 47, 0, '2022-11-17'),
+('GTS232', 'Biology', 10, 47, 0, '2022-11-17'),
+('Pms233', 'Generic Science', 4, 43, 0, '2022-11-17'),
+('Pms235', 'Generic Science', 2, 43, 0, '2022-11-17'),
+('Pms236', '333', 3, 43, 0, '2022-11-17');
+
 -- --------------------------------------------------------
 
 --
@@ -98,6 +115,14 @@ CREATE TABLE `room` (
   `regis_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`room_id`, `building_name`, `building_image`, `room_size`, `admin_id`, `regis_date`) VALUES
+(306, 'Rangsit Campus', '', 22, 0, '2022-11-17'),
+(333, '12th birthday', '', 120, 0, '2022-11-17');
+
 -- --------------------------------------------------------
 
 --
@@ -111,6 +136,16 @@ CREATE TABLE `section` (
   `admin_id` int(10) DEFAULT NULL,
   `regis_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `section`
+--
+
+INSERT INTO `section` (`section_id`, `grade`, `section_size`, `admin_id`, `regis_date`) VALUES
+(2, 10, 12, 38, '2022-11-17'),
+(3, 3, 12, 38, '2022-11-17'),
+(4, 6, 123, 38, '2022-11-17'),
+(5, 10, 26, 38, '2022-11-17');
 
 --
 -- Indexes for dumped tables
@@ -163,7 +198,13 @@ ALTER TABLE `section`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT for table `section`
+--
+ALTER TABLE `section`
+  MODIFY `section_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
